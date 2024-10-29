848564c37ded31c54847f21cdbc1e274a4d632bd
LesyaKot <lesyakot83@gmail.com>
2024-10-29 12:40:22 +0500

    add css to Homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,38 +20,43 @@ export default function Home() {
       <section className={css.wrap}>
         <div className={css.left}>
           <h1 className={css.title}>
-            The road to the depths of the human soul
+            The road to the <span className={css.accent}>depths</span> of the
+            human soul
           </h1>
-          <h3>
+          <h3 className={css.text}>
             We help you to reveal your potential, overcome challenges and find a
             guide in your own life with the help of our experienced
             psychologists.
           </h3>
+          <MainBtn onClick={handleMainBtn}>
+            Get started
+            <ArrowUpRight className={css.arrowIcon}></ArrowUpRight>
+          </MainBtn>
         </div>
+
         <div className={css.right}>
           <img className={css.img} src="/heroImage.png" alt="hero-image" />
 
-          <div>
-            <CheckSquare className={css.iconCheck}></CheckSquare>
+          <div className={css.expPsyco}>
+            <div className={css.checkCont}>
+              <CheckSquare className={css.iconCheck}></CheckSquare>
+            </div>
+            <div className={css.textCont}>
+              <p className={css.expPsycoText}>
+                Experienced psychologists
+                <span className={css.whiteAssent}>15,000</span>
+              </p>
+            </div>
           </div>
 
-          <p className={css.expPsyco}>
-            Experienced psychologists
-            <span className={css.whiteAssent}>15,000</span>
-          </p>
-
-          <div>
+          <div className={css.mainQuestCont}>
             <Question className={css.svgMainQuestion}></Question>
           </div>
-          <div className={css.user}>
-            <People className={css.user}></People>
+
+          <div className={css.people}>
+            <People className={css.peopleIcon}></People>
           </div>
         </div>
-
-        <MainBtn onClick={handleMainBtn}>
-          Get started
-          <ArrowUpRight className={css.user}></ArrowUpRight>
-        </MainBtn>
       </section>
     </>
   );
